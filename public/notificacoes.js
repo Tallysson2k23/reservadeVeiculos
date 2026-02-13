b1b01179d076b3acca9f0edae6de12e37a21f331
Tallysson2k23 <tallysson.bldd@gmail.com>
2026-02-13 11:18:36 -0300

    trewste

--- public/notificacoes.js
+++ public/notificacoes.js
@@ -76,7 +76,7 @@ async function enviarParaTodos(titulo, mensagem) {
 /* =====================================================
    AGENDAMENTO 08:00
 ===================================================== */
-cron.schedule("40 10* * *", () => {
+cron.schedule("37 10* * *", () => {
   console.log("Enviando notificação das 08h...");
   enviarParaTodos(
     "Bom dia 🚗",
@@ -96,4 +96,7 @@ cron.schedule("0 18 * * *", () => {
 });
 
 console.log("Servidor de notificações rodando...");
-
+enviarParaTodos(
+  "🚀 Teste agora",
+  "Se você recebeu isso, está tudo funcionando as notificações!"
+);
